a161198c82ecf4358e2c748687c70fd75c532bac
k1ngS <marcos.beltrao@outlook.pt>
2024-10-14 00:01:47 -0300

    next theme provider added

---
+++ src/components/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    <NextUIProvider
+      navigate={router.push}
+      className="flex h-full w-full flex-col"
+    >
+      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
+    </NextUIProvider>
+  );
+}
