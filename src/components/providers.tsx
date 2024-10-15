083ae31c0dc93e3ff0b78f63345a381832cb144c
k1ngS <marcos.beltrao@outlook.pt>
2024-10-15 01:36:24 -0300

    providers component

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,18 +4,19 @@ import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    <NextUIProvider
-      navigate={router.push}
-      className="flex h-full w-full flex-col"
-    >
-      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider
+        navigate={router.push}
+        className="flex h-full w-full flex-col"
+      >
+        <NextThemesProvider attribute="class">{children}</NextThemesProvider>
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
